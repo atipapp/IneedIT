a0c3e7da1181ca3f079a8fc0618d6a531c09222f
Attila Papp <pppttl@sch.bme.hu>
2018-02-27 18:36:25 +0100

    Show back button on the Request details page

--- app/src/main/java/hu/autsoft/pppttl/ineedit/RequestDetails/RequestDetailsActivity.java
+++ app/src/main/java/hu/autsoft/pppttl/ineedit/RequestDetails/RequestDetailsActivity.java
@@ -3,8 +3,10 @@ package hu.autsoft.pppttl.ineedit.RequestDetails;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import hu.autsoft.pppttl.ineedit.R;
@@ -19,6 +21,12 @@ public class RequestDetailsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +35,16 @@ public class RequestDetailsActivity extends AppCompatActivity {
                 dialog.show(view, getString(R.string.edit_request));
             }
         });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
