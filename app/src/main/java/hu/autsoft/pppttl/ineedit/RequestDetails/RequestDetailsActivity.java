bed3bd6bdb2c9122c3d7bce536b50347930495c7
Attila Papp <pppttl@sch.bme.hu>
2018-03-07 21:05:13 +0100

    Remove test leftover from RequestDetailsActivity

--- app/src/main/java/hu/autsoft/pppttl/ineedit/RequestDetails/RequestDetailsActivity.java
+++ app/src/main/java/hu/autsoft/pppttl/ineedit/RequestDetails/RequestDetailsActivity.java
@@ -93,9 +93,6 @@ public class RequestDetailsActivity extends AppCompatActivity implements Request
         List<Comment> comments = request == null ? new ArrayList<Comment>() : new ArrayList<>(request.getComments());
         adapter = new CommentRecyclerViewAdapter(comments);
 
-        adapter.addComment(new Comment("aa", "Teszt komment"));
-        adapter.addComment(new Comment("aa", "Teszt komment"));
-        adapter.addComment(new Comment("aa", "Teszt komment"));
         recyclerView.setAdapter(adapter);
     }
 
