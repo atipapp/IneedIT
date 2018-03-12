f8873f02b6ace4429ad7536b86ffea4d43337534
Attila Papp <pppttl@sch.bme.hu>
2018-03-12 16:12:50 +0100

    Implement dependency injection in Requests activity

---
+++ app/src/main/java/hu/autsoft/pppttl/ineedit/di/BuildersModule.java
@@ -0,0 +1,18 @@
+package hu.autsoft.pppttl.ineedit.di;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+import hu.autsoft.pppttl.ineedit.Requests.RequestsActivity;
+import hu.autsoft.pppttl.ineedit.Requests.RequestsModule;
+import hu.autsoft.pppttl.ineedit.Requests.RequestsViewModule;
+
+/**
+ * Created by pppttl on 2018. 03. 12..
+ */
+
+@Module
+public abstract class BuildersModule {
+
+    @ContributesAndroidInjector(modules = {RequestsViewModule.class, RequestsModule.class})
+    abstract RequestsActivity bindLobbyActivity();
+}
