0805e1cc28339ebff40763e9c68c56ac39953ad3
Attila Papp <pppttl@sch.bme.hu>
2018-03-27 18:02:48 +0200

    Implement userID - email switching table

--- app/src/main/java/hu/autsoft/pppttl/ineedit/login/LoginInteractorImpl.java
+++ app/src/main/java/hu/autsoft/pppttl/ineedit/login/LoginInteractorImpl.java
@@ -8,7 +8,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -17,7 +18,9 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginInteractorImpl implements LoginContract.LoginInteractor {
     private static final String TAG = "LoginInteractorImpl";
+    private static final String USERS_NAME = "users";
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseUser currentUser;
 
     @Override
     public void login(final String username, final String password, final OnLoginFinishedListener listener) {
@@ -28,7 +31,8 @@ public class LoginInteractorImpl implements LoginContract.LoginInteractor {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
+                            currentUser = mAuth.getCurrentUser();
+                            saveCurrentUserEmail();
                             listener.onSuccess();
                         } else {
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
@@ -41,5 +45,14 @@ public class LoginInteractorImpl implements LoginContract.LoginInteractor {
     @Override
     public boolean isLoggedIn() {
         return mAuth.getCurrentUser() != null;
+    }
+
+    private void saveCurrentUserEmail() {
+        if (currentUser != null) {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference databaseReference = database.getReference();
+
+            databaseReference.child(USERS_NAME).child(currentUser.getUid()).setValue(currentUser.getEmail());
+        }
     }
 }
