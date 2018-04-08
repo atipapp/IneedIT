b20c60b5cbb5f4e6b5adf651951903a7ce27cdb1
Attila Papp <pppttl@sch.bme.hu>
2018-04-08 17:08:44 +0200

    Add User saving MaterialDialog

---
+++ app/src/main/java/hu/autsoft/pppttl/ineedit/useredit/UserEditDialog.java
@@ -0,0 +1,45 @@
+package hu.autsoft.pppttl.ineedit.useredit;
+
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import hu.autsoft.pppttl.ineedit.R;
+import hu.autsoft.pppttl.ineedit.model.User;
+
+/**
+ * Created by pppttl on 2018. 04. 08..
+ */
+public class UserEditDialog {
+    public void show(String title, View view, final User user, final SaveUserCallbackListener listener) {
+        boolean wrapInScrollView = true;
+        final MaterialDialog addDialog = new MaterialDialog.Builder(view.getContext())
+                .title(title)
+                .customView(R.layout.dialog_user_edit, wrapInScrollView)
+                .positiveText(R.string.save)
+                .negativeText(R.string.cancel)
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        View view = dialog.getView();
+                        String fullName = ((EditText) view.findViewById(R.id.userFullName)).getText().toString();
+                        Boolean useLoginAddressForWorkEmail = ((CheckBox) view.findViewById(R.id.user_work_email_prompt)).isChecked();
+                        String workEmail = !useLoginAddressForWorkEmail ? user.getEmail() : ((EditText) view.findViewById(R.id.userWorkEmail)).getText().toString();
+                        String phoneNumber = ((EditText) view.findViewById(R.id.userPhoneNumber)).getText().toString();
+                        String workAddress = ((EditText) view.findViewById(R.id.userWorkAddress)).getText().toString();
+
+                        user.setFullName(fullName);
+                        user.setWorkEmail(workEmail);
+                        user.setPhoneNumber(phoneNumber);
+                        user.setWorkAddress(workAddress);
+                        if (listener != null) listener.onSaveUser(user);
+                    }
+                })
+                .show();
+
+    }
+}
