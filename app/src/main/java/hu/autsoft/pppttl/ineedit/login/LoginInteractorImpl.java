4f3fc1307efbf5ebce07e9b88d6b61bd860d7276
Attila Papp <pppttl@sch.bme.hu>
2018-04-08 20:31:33 +0200

    Save default user info upon login if it doesn't exist

--- app/src/main/java/hu/autsoft/pppttl/ineedit/login/LoginInteractorImpl.java
+++ app/src/main/java/hu/autsoft/pppttl/ineedit/login/LoginInteractorImpl.java
@@ -8,8 +8,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import hu.autsoft.pppttl.ineedit.model.User;
 
 
 /**
@@ -21,6 +26,7 @@ public class LoginInteractorImpl implements LoginContract.LoginInteractor {
     private static final String USERS_NAME = "users";
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private FirebaseUser currentUser;
+    private User user;
 
     @Override
     public void login(final String username, final String password, final OnLoginFinishedListener listener) {
@@ -50,9 +56,25 @@ public class LoginInteractorImpl implements LoginContract.LoginInteractor {
     private void saveCurrentUserEmail() {
         if (currentUser != null) {
             FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference databaseReference = database.getReference();
+            final DatabaseReference databaseReference = database.getReference().child(USERS_NAME).child(currentUser.getUid());
+            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    user = dataSnapshot.getValue(User.class);
+                    if (user == null) {
+                        user = new User();
+                        user.setuID(currentUser.getUid());
+                        user.setEmail(currentUser.getEmail());
+                        user.setWorkEmail(currentUser.getEmail());
+                        databaseReference.setValue(user);
+                    }
+                }
 
-            databaseReference.child(USERS_NAME).child(currentUser.getUid()).setValue(currentUser.getEmail());
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
     }
 }
