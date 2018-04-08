bf2d00d4ad2a27425ee6296e792218ed8ae029bf
Attila Papp <pppttl@sch.bme.hu>
2018-04-08 17:08:44 +0200

    Add User saving MaterialDialog

---
+++ app/src/main/java/hu/autsoft/pppttl/ineedit/useredit/SaveUserCallbackListener.java
@@ -0,0 +1,10 @@
+package hu.autsoft.pppttl.ineedit.useredit;
+
+import hu.autsoft.pppttl.ineedit.model.User;
+
+/**
+ * Created by pppttl on 2018. 04. 08..
+ */
+public interface SaveUserCallbackListener {
+    void onSaveUser(User user);
+}
