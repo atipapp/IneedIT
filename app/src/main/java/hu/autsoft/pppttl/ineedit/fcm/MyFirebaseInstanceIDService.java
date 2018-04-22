ec9c3708f014a47bdf1fe4eb719889b6e310206c
Attila Papp <pppttl@sch.bme.hu>
2018-04-22 17:14:59 +0200

    Initialize Firebase Cloud Messaging

---
+++ app/src/main/java/hu/autsoft/pppttl/ineedit/fcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,25 @@
+package hu.autsoft.pppttl.ineedit.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by pppttl on 2018. 04. 22..
+ */
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+
+        //For registration of token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //To displaying token on logcat
+        Log.d("TOKEN: ", refreshedToken);
+
+    }
+
+}
